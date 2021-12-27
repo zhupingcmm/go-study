b71255a932bcd1e857321d6a4e614fa901256782
pingzhu <ping.zhu@microfocus.com>
2021-12-27 10:50:33 +0800

    add module2

---
+++ src/examples/module2/mutex/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	go lock()
+	go rLock()
+	go wLock()
+	time.Sleep(5* time.Second)
+
+}
+
+func lock() {
+	lock := sync.Mutex{};
+	for i := 0; i < 3; i++ {
+		lock.Lock()
+		defer lock.Unlock()
+		fmt.Println("lock:", i)
+	}
+}
+
+func rLock () {
+	lock := sync.RWMutex{}
+	for i :=0; i<3; i++ {
+		lock.RLock()
+		defer lock.RUnlock()
+		fmt.Println("rLock:", i)
+	}
+}
+
+func wLock () {
+	lock := sync.RWMutex{}
+
+	for i := 0; i < 3; i++ {
+		lock.Lock()
+		defer lock.Unlock()
+		fmt.Println("wLock:", i)
+	}
+}
